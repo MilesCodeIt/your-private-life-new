0467bc177924dda148df04bd0610eb1664f27ed8
Mikkel RINGAUD <mikkel@milescode.dev>
2023-03-11 22:20:35 +0100

    chore: add dashboard page

---
+++ src/routes/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const DashboardHomePage: Component = () => {
+  return (
+    <div>
+      Hello World!
+    </div>
+  );
+};
+
+export default DashboardHomePage;
