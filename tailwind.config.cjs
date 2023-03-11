94cdc6f94f63f9c025ded5c63e6f1404b53fd93a
Mikkel RINGAUD <mikkel@milescode.dev>
2023-03-11 14:28:29 +0100

    feat: add `Fira Code` font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      mono: ["Fira Code", "monospace"]
+    }
   },
   plugins: []
 };
